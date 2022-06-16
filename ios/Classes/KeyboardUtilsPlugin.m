f786a5a63139f1d1967b4ccd92e52fc1c46c1fb1
Nguyen Truong Vu <doit.programer@gmail.com>
2022-06-16 17:18:11 +0700

    Update KeyboardUtilsPlugin.m

--- ios/Classes/KeyboardUtilsPlugin.m
+++ ios/Classes/KeyboardUtilsPlugin.m
@@ -1,5 +1,12 @@
 #import "KeyboardUtilsPlugin.h"
+#if __has_include(<keyboard_utils/keyboard_utils-Swift.h>)
 #import <keyboard_utils/keyboard_utils-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import <keyboard_utils-Swift.h>
+#endif
 
 @implementation KeyboardUtilsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
